fc158ce6fc3614a769da3e76949ea21fdc93205d
Tomohiro Nakamura <quickness.net@gmail.com>
2015-07-28 21:47:43 +0900

    updat PoshGit2.psm to update public Get-GitRepository

--- PoshGit2.psm1
+++ PoshGit2.psm1
@@ -3,9 +3,10 @@ If(-not (Get-Module PoshUtil)) {
     return
 }
 
+. (Join-Paths $PSScriptRoot Lib Get-GitRepository.ps1)
 . (Join-Paths $PSScriptRoot Lib Invoke-Git.ps1)
 
 New-Alias -Name git -Value Invoke-Git
 
-Export-ModuleMember -Function Invoke-Git
+Export-ModuleMember -Function Invoke-Git, Get-GitRepository
 Export-ModuleMember -Alias git
