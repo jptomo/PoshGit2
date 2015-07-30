e938e97794521580edc5470646e8de4ac888c52c
Tomohiro Nakamura <quickness.net@gmail.com>
2015-07-30 21:42:30 +0900

    update git clone wip

--- Lib/Invoke-GitClone.ps1
+++ Lib/Invoke-GitClone.ps1
@@ -1,3 +1,17 @@
+. (Join-Path $PSScriptRoot _Util.ps1)
+
 Function Invoke-GitClone
 {
+
+# TODO: Uri と Path をハンドルするようにする
+    Process
+    {
+        $params = (
+            Resolve-Args `
+            -Arguments $Args `
+            -Defaults @{'Depth'=0; 'Branch'=''; 'Uri'=''; 'Path'=''} `
+            -KeyMaps @{'Depth'='--depth'; 'Branch'='-b','--branch'})
+        $params
+    }
+
 }
