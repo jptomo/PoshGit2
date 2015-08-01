f90903c5abdb05f63ea149f4ab1cbb1903776509
Tomohiro Nakamura <quickness.net@gmail.com>
2015-08-01 23:57:16 +0900

    update del Useless Join-Paths

--- Lib/PoshUtil.ps1
+++ Lib/PoshUtil.ps1
@@ -130,7 +130,7 @@ Function New-TempDirectory
     {
         Do
         {
-            $path = (Join-Paths $ENV:TMP "${Prefix}$(Get-RandomText)")
+            $path = (Join-Path $ENV:TMP "${Prefix}$(Get-RandomText)")
         }
         While(Test-Path $path)
 
