70c8bcf79656ad7372108b0b200b222905e788a8
Tomohiro Nakamura <quickness.net@gmail.com>
2015-08-02 01:11:50 +0900

    update clone to resolve dst path

--- Lib/Invoke-GitClone.ps1
+++ Lib/Invoke-GitClone.ps1
@@ -11,7 +11,27 @@ Function Invoke-GitClone
                 -KeyMaps @{'Depth'='--depth'; 'Branch'='-b','--branch'} `
                 -Positionals 'Uri','Path')
 
-        $path = (Convert-FullPath $params['Path'])
+        If($params['Path'] -Eq '')
+        {
+            If($params['Uri'] -Match '/')
+            {
+                $last = ($params['Uri'] -Split '/' | ? {$_ -Ne '' } | select -Last 2)
+            }
+            Else
+            {
+                $last = ($params['Uri'] -Split '\' | ? {$_ -Ne '' } | select -Last 2)
+            }
+            If($last[1])
+            {
+                $dstPath = $last[1]
+            }
+            Else
+            {
+                $dstPath = $last[0]
+            }
+            $dstPath = ($dstPath -Replace '.git$', '')
+        }
+        $dstPath = (Convert-FullPath $dstPath)
 
         $options = (New-Object LibGit2Sharp.CloneOptions)
         If($params['Branch'] -Ne '')
@@ -24,6 +44,9 @@ Function Invoke-GitClone
             # TODO: Impl: Depth Option
         }
 
-        [LibGit2Sharp.Repository]::Clone($params['Uri'], $path, $options)
+        $tmpDir = New-TempDirectory
+        [LibGit2Sharp.Repository]::Clone($params['Uri'], $tmpDir, $options)
+
+        Move-Item -Path $tmpDir -Destination $dstPath
     }
 }
