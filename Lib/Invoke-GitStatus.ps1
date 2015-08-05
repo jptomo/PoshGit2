8a2869b6df7d62fa59c736f95a79704e85e89d9b
Tomohiro Nakamura <quickness.net@gmail.com>
2015-08-05 20:04:29 +0900

    wip

--- Lib/Invoke-GitStatus.ps1
+++ Lib/Invoke-GitStatus.ps1
@@ -13,10 +13,28 @@ Function Invoke-GitStatus
             return
         }
 
+        $status2text =
+        {
+            Param(
+                [LibGit2Sharp.StatusEntry]$item
+            )
+
+            Process
+            {
+                $entries = [LibGit2Sharp.StatusEntry]
+                $statusDict = @{
+                    'Modified' = 'M ';
+                    'ModifiedInWorkdir' = ' M';
+                }
+
+                return $statusDict[$item.state.ToString()]
+            }
+        }
+
         $options = (New-Object LibGit2Sharp.StatusOptions)
         ForEach($item In $repo.RetrieveStatus($options))
         {
-            Write-Host $item.filePath
+            Write-Host "$(&$status2text $item) $($item.filePath)"
         }
     }
 }
